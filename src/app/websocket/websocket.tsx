f1add71ccf2c45f05dc6221ccdbe5a51012f6e27
lecterkn <lecterkn@proton.me>
2024-10-21 14:38:16 +0900

    websocketのイベントを追加

---
+++ src/app/websocket/websocket.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { UUID } from "crypto";
+import Cookies from "js-cookie";
+import { MessageEntity } from "../api/response/schema";
+import { on_add_message, on_delete_message, on_update_message } from "./handlers/message_handler";
+
+const jwtToken = () => {
+  return Cookies.get("token");
+};
+
+interface EventMessage {
+  src: UUID;
+  event: string;
+  message: string;
+}
+
+export async function ws_handler() {
+  const token = jwtToken();
+  if (token == undefined) {
+    console.log("token is undefined");
+    return;
+  }
+  const socket = new WebSocket("ws://localhost:8891");
+  // JWT認証
+  socket.onopen = function (event) {
+    socket.send(token);
+    console.log("waiting for ws server...");
+  };
+  socket.onmessage = handle_event
+}
+
+function handle_event(event: MessageEvent<any>) {
+    if (event.data == jwtToken()) {
+      console.log("websocket connected");
+      return;
+    }
+    console.log("websocket receveid: " + event.data);
+    // イベントメッセージに変換
+    const messageEvent: EventMessage = JSON.parse(event.data);
+    // 新規メッセージの場合
+    if (messageEvent.event == "add_message") {
+      const message: MessageEntity = JSON.parse(messageEvent.message);
+      on_add_message(message);
+    }
+    else if (messageEvent.event == "update_message") {
+      const message: MessageEntity = JSON.parse(messageEvent.message);
+      on_update_message(message);
+    }
+    else if (messageEvent.event == "delete_message") {
+      const message: MessageEntity = JSON.parse(messageEvent.message);
+      on_delete_message(message);
+    }
+}
